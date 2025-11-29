2c9dfd2dab40ac4e1c9e56d22c1c53f9ad3c48a3
nguyenvanhieu <nguyenvanhieu@NGVANHIEU>
2025-11-29 11:54:47 +0700

    nop bai

---
+++ 123/Program.c
@@ -0,0 +1,59 @@
+// CConsoleApplication.c : file nay chua 'main' function. 
+// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
+
+#include <stdio.h>
+
+int main()
+{
+int intergerArray[50];
+int i;
+int length;
+int tmp;//temp
+printf("Nhap so luong phan tu mang: ");
+scanf("%d", &length);
+printf("Nhap du lieu mang %d phan tu:\n ", length);
+for (i = 0; i < length; i++)
+{
+    printf("mang[%d] = ", i);
+    scanf("%d", &intergerArray[i]);
+}
+printf("Xuat du lieu mang %d phan tu :\n", length);
+for (i = 0; i < length; i++);
+{
+    if (intergerArray[i] > intergerArray[i + 1])
+    {
+        tmp = intergerArray[1];
+        intergerArray[i] = intergerArray[i + 1];
+        intergerArray[i + 1] = tmp;
+        i = -1;
+    }
+
+}
+printf("Xuat du lieu mang %d phan tu Giam dan\n", length);
+for (i = length - 1; i > -1; i--)
+{
+    printf("mang[%d] = %d\n", i, intergerArray[i]);
+}
+printf("Xuat dua lieu mang %d phan tu Giam dan\n", length);
+for (i = 0; i < length; i++)
+{
+    printf("mang[%d] = %d\n", i, intergerArray[i]);
+
+
+}
+}
+
+// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
+
+// Danh cho nguoi moi bat dau:
+//   1. Dung "Solution Explorer window" de them/quan ly cac files
+//   2. Dung "Output window" de kiem tra "build output" va "cac thong bao khac"
+//   3. Dung "Error List window" de xem loi
+//   4. Chon "Project > Add New Item" de tao moi file code,
+//      hoac "Project > Add Existing Item" de them file da ton tai vao project nay.
+//   5. Sau nay, de mo lai project nay,
+//      chon "File > Open > Project" va chon file *.sln
+//      Co the xem thu muc project, noi luu file *.sln bang cach
+//      bam phim chuot phai vao "Solution" > chon "Open Folder in File Explorer"
+
+// GV: AnhTT184
